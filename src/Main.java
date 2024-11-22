0e026224505ab6af4ca610a2dbb89f0b21d09099
fabionc <fabionc@unicamp.br>
2024-11-22 13:42:06 -0300

    Update class filme

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Imersão");
 
-        System.out.println("Hello, World!");
+        int anoDeLancamento = 2024;
+        System.out.println("Ano de Lançamento: "  + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println(media);
+
+        String sinopse;
+
+        sinopse = "Filme de misterio com Leonardo";
+
+        System.out.println((sinopse));
     }
 }
